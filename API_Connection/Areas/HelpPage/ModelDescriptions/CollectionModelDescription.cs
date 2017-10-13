ba7a24c6ba91f93ee96aa7892a65145764299ba3
Unknown <thebloodycrown@gmail.com>
2017-10-13 11:30:09 +0200

    init

---
+++ API_Connection/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace API_Connection.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
